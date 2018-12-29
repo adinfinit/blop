a15762429f0ca2c6ca0e93656970e68ccfc4aa50
Egon Elbre <egonelbre@gmail.com>
2018-12-29 17:10:09 +0200

    Add animated ground

---
+++ Assets/Scripts/RandomizeAnimationStart.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizeAnimationStart: MonoBehaviour
+{
+    void Start()
+    {
+        var animator = GetComponent<Animator>();
+        animator.Play(0, -1, Random.Range(0.0f, 1.0f));
+    }
+}
---
+++ Assets/Scripts/RandomizeColor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizeColor : MonoBehaviour
+{
+    void Start()
+    {
+        var r = GetComponent<SpriteRenderer>();
+        var h = 0.3f;
+        r.color = Random.ColorHSV(h, h + 0.1f, 0.6f, 0.7f, 0.7f, 0.8f);
+    }
+}
